04fae9e06bb0c229ba75d2769cc5088dd60da2b8
wangcheng <wangcheng4203411@163.com>
2018-12-20 17:50:52 +0800

    testGame

---
+++ testGame/assets/Script/HelloWorld.js
@@ -0,0 +1,22 @@
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        label: {
+            default: null,
+            type: cc.Label
+        },
+        // defaults, set visually when attaching this script to the Canvas
+        text: 'This is the first game!'
+    },
+
+    // use this for initialization
+    onLoad: function () {
+        this.label.string = this.text;
+    },
+
+    // called every frame
+    update: function (dt) {
+
+    },
+});
